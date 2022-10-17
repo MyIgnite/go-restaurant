8e5041fd784d66abec848c07452c8d4ebfcac5d6
Renan Moreira <renan.tsx@gmail.com>
2022-10-17 11:23:12 -0300

    resolved Input

--- src/components/ModalAddFood/index.jsx
+++ src/components/ModalAddFood/index.jsx
@@ -1,9 +1,9 @@
 import { Component, createRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
-import { Form } from './styles';
+import { Input } from '../Input';
 import Modal from '../Modal';
-import Input from '../Input';
+import { Form } from './styles';
 
 class ModalAddFood extends Component {
   constructor(props) {
