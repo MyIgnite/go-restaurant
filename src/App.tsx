01eeb935bbcf7949450ed0ba3a9af0a057c2a62f
Renan Moreira <renan.tsx@gmail.com>
2022-10-17 18:21:32 -0300

    resolved ModalEditFood and api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import Routes from './routes';
-
 import GlobalStyle from './styles/global';
 
-const App = () => (
-  <>
+export function App() {
+  return (
+    <>
     <GlobalStyle />
     <Router>
       <Routes />
     </Router>
   </>
-);
-
-export default App;
+  )
+}
